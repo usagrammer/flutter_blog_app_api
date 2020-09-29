62ef371c212c2570d69f2d583c683c0f3576d44d
usagrammer <uny0102@gmail.com>
2020-09-29 17:31:41 +0900

    try delete routes for options

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :articles
   resources :categories
 
-  match '*path' => 'options_request#preflight', via: :options
+  # match '*path' => 'options_request#preflight', via: :options
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
