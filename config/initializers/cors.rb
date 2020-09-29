99100c4840548950b40c4a0a6fdc0e08d0f09b8c
usagrammer <uny0102@gmail.com>
2020-09-29 17:27:34 +0900

    change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['192.168.1.16']
+    origins ['localhost:53874']
 
     resource '*',
       headers: :any,
