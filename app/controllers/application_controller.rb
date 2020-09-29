b371f37a32dce58f95d8739ad6d1f3f047ae182a
usagrammer <uny0102@gmail.com>
2020-09-29 18:44:52 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::API
   protected
   def authenticate
     authenticate_or_request_with_http_token do |token, options|
+      puts "token:>>>#{token}"
+      puts "options:>>>#{options}"
       token == 'hoge'
     end
   end
